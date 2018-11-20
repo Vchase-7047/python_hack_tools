cb34fdc2d53a8bb9ba5348f3a1d73a8987fbc7b6
Vchase <39075477+Vchase-7047@users.noreply.github.com>
2018-11-21 00:44:09 +0800

    模拟SSH登录并进行命令输入

---
+++ Pexpect_SSH/use_Pexpect_SSH.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import pexpect
+
+PROMPT = ['# ', '>>> ', '> ', '\$ ']
+def send_command(child, cmd):
+    child.sendline(cmd)   # 连接成功后发送命令
+    child.expect(PROMPT)  # 使用expect来捕捉字符
+    print child.before
+
+
+def connect(user, host, password):
+    ssh_newkey = 'Are you sure want to continue connecting'
+    connStr = user + '@' + host
+    child = pexpect.spawn('/usr/bin/ssh', [connStr])  # 第一个参数是启动命令的程序，第二个是的传入该程序的命令
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])  # 捕捉字符符合就返回1
+    if ret == 0:
+        print '[-] Error Connecting'
+        return
+    if ret == 1:
+        child.sendline('yes')
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+    if ret == 0:
+        print '[-] Error Connecting'
+        return
+    child.sendline(password)
+    child.expect(PROMPT)
+    return child
+
+
+def main():
+    # host = '47.101.150.236'
+    # user = 'root'
+    # password = 'XZHh15731#'
+    host = '10.10.10.130'
+    user = 'vchase'
+    password = 'vchase'
+    child = connect(user, host, password)
+    send_command(child, 'ls')
+
+
+if __name__ == '__main__':
+    main()
